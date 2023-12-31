b1dcae3e02e70cd1a02cedeeec6fdd0fa8aaa1ac
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-12-31 18:26:15 -0300

    feat: `props 3` react typescript

--- basico/props/src/App.tsx
+++ basico/props/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 
 export const App = () => {
@@ -11,6 +12,9 @@ export const App = () => {
   }
 
   return (
-    <div><p>Total: {total}</p><Button style={{ fontSize: "24px" }} onClick={increment}>Incrementar</Button></div>
+    <div><p>Total: {total}</p><Button style={{ fontSize: "24px" }} onClick={increment}>Incrementar</Button>
+      <Input label="Nome" id="nome" />
+      <Input label="Email" id="email" />
+    </div>
   )
 }
