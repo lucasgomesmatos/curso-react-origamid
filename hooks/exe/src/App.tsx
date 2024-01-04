ece663cc119f1d0f29d9bd807237b03588e8ae00
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-01-03 23:21:29 -0300

    feat: `exercicio finalizado` react typescript

--- hooks/exe/src/App.tsx
+++ hooks/exe/src/App.tsx
@@ -1,3 +1,63 @@
+import { useEffect, useState } from 'react';
+import { Input } from './components/Input';
+
+interface Vendas {
+  id: string;
+  nome: string;
+  preco: number;
+  status: string;
+  pagamento: string;
+  parcelas: number | null;
+  data: string;
+}
+
 export const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState({
+    inicio: '',
+    fim: '',
+  });
+
+  const [response, setResponse] = useState<Vendas[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  async function getData(inicio: string, final: string) {
+    const response = await fetch(
+      `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`,
+    );
+    const json = await response.json();
+    setResponse(json);
+  }
+
+  useEffect(() => {
+    if (data.inicio && data.fim) {
+      getData(data.inicio, data.fim);
+    }
+  }, [data]);
+
+  return (
+    <div>
+      <Input onChange={handleChange} label="Início" id="inicio" type="date" />
+      <Input onChange={handleChange} label="Fim" id="fim" type="date" />
+
+      <ul>
+        {response &&
+          response.map((venda) => (
+            <li key={venda.id}>
+              {venda.nome} -{' '}
+              {venda.preco.toLocaleString('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </li>
+          ))}
+        {response.length === 0 && <li>Nenhum dado encontrado</li>}
+      </ul>
+    </div>
+  );
 };
--- hooks/exe/src/components/Input.tsx
+++ hooks/exe/src/components/Input.tsx
@@ -2,13 +2,14 @@ import { ComponentProps } from 'react';
 
 interface InputProps extends ComponentProps<'input'> {
   label: string;
+  id: string;
 }
 
 export const Input = ({ label, id, ...rest }: InputProps) => {
   return (
-    <div>
+    <div style={{ marginBottom: '1rem' }}>
       <label htmlFor={id}>{label}</label>
-      <input type="text" {...rest} />
+      <input name={id} id={id} {...rest} />
     </div>
   );
 };
